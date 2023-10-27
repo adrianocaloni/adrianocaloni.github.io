b88b2010e94622d842b6442213d82bcc9ce845b8
Adriano Caloni <caloniadrianoj@gmail.com>
2023-10-27 07:14:13 -0300

    Se agrega la funcionalidad para borrar

--- javascript.js
+++ javascript.js
@@ -3,10 +3,31 @@ const precioPorTicket = 200;
 function obtenerValor(){
 var cantidadTicket = document.getElementById("cantidadNumero").value;
 var descuentoTotal = document.getElementById("cantidadDescuento").value;
+
+if (cantidadTicket < "0"){
+
+    var muestroResultado = document.getElementById("totalFianl");
+    muestroResultado.textContent = "Tiene que seleccionar la cantidad de TICKET"
+}
+else if (descuentoTotal < "0") {
+    var muestroResultado = document.getElementById("totalFianl");
+    muestroResultado.textContent = "Debe seleccionar una Categoría"
+
+}
+else
+{
 var subTotal = precioPorTicket * cantidadTicket;
 var descuentoFinal = subTotal * descuentoTotal / 100;
 var percioFinal = subTotal - descuentoFinal 
 
 var muestroResultado = document.getElementById("totalFianl");
 muestroResultado.textContent = "Total a Pagar: $"+percioFinal;
+}
+}
+
+function borrarValor(){
+
+    var muestroResultado = document.getElementById("totalFianl");
+    muestroResultado.textContent = "Total a Pagar: $0"
+
 }
